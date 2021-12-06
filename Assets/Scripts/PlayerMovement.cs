950bf0ccaa4d59546c93c46edc2f32bfe4581562
Mohssine Achak <achak.mohssine@gmail.com>
2021-12-06 14:21:52 +0100

    Change collide check origin point from center to bottom

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,13 +14,16 @@ public class PlayerMovement : MonoBehaviour
     Vector2 velocity;
 
     [SerializeField] private float groundRadius = .1f;
-    [SerializeField] private LayerMask groundMask ;
+    [SerializeField] private LayerMask groundMask;
 
     private RaycastHit2D[] hits = new RaycastHit2D[1];
+
+    private Bounds playerBounds;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerBounds = GetComponent<Collider2D>().bounds;
         velocity = new Vector2(0, upwardVelocity);
         step = movementSpeed * Time.fixedDeltaTime;
     }
@@ -49,14 +52,17 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(inputX * step, 0, 0);
     }
 
-    private bool IsGrounded() 
+    private bool IsGrounded()
     {
-        return Physics2D.CircleCastNonAlloc(transform.position,groundRadius,Vector2.down,hits,.1f,groundMask) > 0;
+        var position = transform.position;
+        return Physics2D.CircleCastNonAlloc(
+            new Vector2(position.x, position.y - playerBounds.extents.y), groundRadius,
+            Vector2.down, hits, .1f, groundMask) > 0;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             animator.SetBool("IsJumping", false);
         }
