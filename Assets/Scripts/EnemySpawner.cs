9331150d8e5924f100cba529544fcd5d632a297b
Taryn M <taryn.mcmillan@gmail.com>
2021-12-16 11:57:37 -0500

    Add in ability to create enemy waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] int startingWave = 0;
+    bool looping = false;
+
+    AudioSource audioSource;
+    int score;
+
     void Start()
     {
-        
+        StartCoroutine(SpawnEnemies());
+    }
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+            {
+                var currentWave = waveConfigs[waveIndex];
+                yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
     {
-        
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(), transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
     }
 }
