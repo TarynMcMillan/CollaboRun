dc6cffa50f0b865074ab37498ce346c5e22396d2
Taryn M <taryn.mcmillan@gmail.com>
2021-12-16 11:31:26 -0500

    Add public methods to WaveConfig

--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
 public class WaveConfig : ScriptableObject
 {
-   // Used to configure waves of enemies. More to come!
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float timeBetweenSpawns = 1f;
+    [SerializeField] int numberOfEnemies = 5;
+    public int damageDealt;
 
-    void Update()
+    public GameObject GetEnemyPrefab()
     {
-        
+        return enemyPrefab;
+    }
+    public int GetNumberOfEnemies()
+    {
+        return numberOfEnemies;
+    }
+    public float GetTimeBetweenSpawns()
+    {
+         return timeBetweenSpawns;
     }
 }
