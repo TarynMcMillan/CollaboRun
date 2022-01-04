ec5000520be7e3864ce9b7cbfec3f65093dc156f
Taryn M <taryn.mcmillan@gmail.com>
2022-01-04 09:39:42 -0500

    Change background from a quad to a sprite

--- Assets/Scripts/ObstalceGenerator.cs
+++ Assets/Scripts/ObstalceGenerator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnvironmentGenerator : MonoBehaviour
+public class ObstalceGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class EnvironmentGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
 }
--- Assets/Scripts/SceneScroller.cs
+++ Assets/Scripts/SceneScroller.cs
@@ -4,17 +4,51 @@ using UnityEngine;
 
 public class SceneScroller : MonoBehaviour
 {
-    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    
     Material myMaterial;
     Vector2 offSet;
+    public List<GameObject> obstacles = new List<GameObject>();
+    public Transform[] spawnPos;
+
+    // test
+    public BoxCollider2D collider;
+    public Rigidbody2D rb;
+    private float width;
+    public float scrollSpeed;
 
     void Start()
     {
-        myMaterial = GetComponent<Renderer>().material;
-        offSet = new Vector2(backgroundScrollSpeed, 0f);
+        //myMaterial = GetComponent<Renderer>().material;
+        //offSet = new Vector2(backgroundScrollSpeed, 0f);
+        //SpawnObstacle();
+
+        collider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        width = collider.size.x;
+        collider.enabled = false;
+
+        rb.velocity = new Vector2(scrollSpeed, 0);
+
+        
     }
     void Update()
     {
-        myMaterial.mainTextureOffset += offSet * Time.fixedDeltaTime;
+        if (transform.position.x < -width)
+        {
+            Vector2 resetPos = new Vector2(width * 2f, 0);
+            transform.position = (Vector2)transform.position + resetPos;
+        }
+        //myMaterial.mainTextureOffset += offSet * Time.fixedDeltaTime;
+        
+       
+    }
+
+    void SpawnObstacle()
+    {
+        int randomObstacle = Random.Range(0, obstacles.Count);
+        int randomPos = Random.Range(0, spawnPos.Length);
+        GameObject obstacleInstance = Instantiate(obstacles[randomObstacle], spawnPos[randomPos].position, Quaternion.identity);
+        obstacleInstance.transform.SetParent(spawnPos[randomPos], false);
     }
 }
